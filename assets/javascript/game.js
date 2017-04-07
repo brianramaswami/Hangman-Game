916cae088feba7058c5df1d54e3aa8fce13b4320
brian ramaswami <brian ramaswami>
2017-04-06 21:47:31 -0700

    modified files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 //array of potential words
-var potentialWordsArray = ["madonna", "blue", "purple", "california"];
+// add as many words as you would like
+var potentialWordsArray = ["madonna", "blue", "purple", "california", "computer"];
 
 // array of spaces of word.
 var initialWord;
@@ -26,14 +27,22 @@ var win_Count = 0;
 //Guesses remaining
 var guesses_Remaining = 15;
 
+var key;
+
 //letter 
 var letter;
 
 //controls the game status
 var game = "on";
 
-//
+//controls if guessed right
 var guessedRight = 0;
+
+//controls for if won game
+var didWin = "";
+
+//controls for if lost game
+var didLose = "";
 
 //function that randomly takes a word from potentialWordsArray and makes spaces for that number of characters
 function pickWord(array) {
@@ -46,23 +55,26 @@ function pickWord(array) {
 
 //function that sets array of guessed letters and the array of the word.
 function setGuessedArray() {
-	var letter = prompt("guess a letter!");
-	guessedRight = 0;
-	guessedLetters.push(letter);
-	console.log(guessedLetters);
-	for (var i = 0; i < wordLength; i++) {
-		if (letter == initialWord.charAt(i)) {
-			initialWordArray[i] = letter;
-			displayWords();
-			guessedRight = 1; //guessed right
-			console.log(initialWordArray); //testing
-		} else{
-			document.getElementById("array of guessed letters").innerHTML = guessedLetters.join(' ');
+	var letter = prompt("Guess A Letter!");
+	// letter = event.key;
+	if (letter != 1 && letter != 2) {
+		guessedRight = 0;
+		guessedLetters.push(letter);
+		console.log(guessedLetters);
+		for (var i = 0; i < wordLength; i++) {
+			if (letter == initialWord.charAt(i)) {
+				initialWordArray[i] = letter;
+				displayWords();
+				guessedRight = 1; //guessed right
+				console.log(initialWordArray); //testing
+			} else{
+				document.getElementById("array of guessed letters").innerHTML = guessedLetters.join(' ');
+			}
 		}
-	}
-	if (guessedRight != 1) {
-		guesses_Remaining--;
-		guessesRemaining();	
+		if (guessedRight != 1) {
+			guesses_Remaining--;
+			guessesRemaining();	
+		}
 	}
 }
 
@@ -73,6 +85,8 @@ function resetValues() {
 	positionCount  = 0;
 	positionCount1 = 0;
 	guesses_Remaining = 15;
+	didWin = "False";
+	didLose = "False";
 }
 
 //display the spaces for word
@@ -92,15 +106,12 @@ function guessesRemaining() {
 
 function ResultLoss() {
 	if(guesses_Remaining == 0){
-		alert("YOU LOSE");
-		alert("Press 0 to start over");
+		// alert("YOU LOSE");
+		// alert("Press 0 to start over");
+		didLose = "True";
+		document.getElementById("instructions").innerHTML =  "YOU LOSE! Press 0 to start over!";
 	}
 }
-//reset wins
-function resetWins() {
-	win_Count = 0;
-}
-
 
 //check if won
 function ResultWin() {
@@ -113,45 +124,72 @@ function ResultWin() {
 		} 
 	}	 
 	if(positionCount == 1 && positionCount1 == 0){
-		alert("YOU WON")
+		// alert("YOU WON")
 		win_Count++
 		winCount();
-		alert("Press 1 to generate new word or 0 to start over");
+		// alert("Press 1 to generate new word or 0 to start over");
+		didWin = "True";
+		document.getElementById("instructions").innerHTML =  "YOU WON!! Press 1 To Generate New Word Or 0 To Start Over";
 	}
 }
+
+//reset wins
+function resetWins() {
+	win_Count = 0;
+}
+
+//ResultTie
+function ResultTie() {
+		if (didLose == "False" && didWin == "False") {
+			setGuessedArray();
+		}
+	}	
+
+//check Result
+function checkResult() {
 	
+}
+
+//new word
+function newWord() {
+	resetValues();
+	winCount();
+	pickWord(potentialWordsArray);
+	console.log(initialWord);
+	displayWords();
+	guessesRemaining();
+	document.getElementById("array of guessed letters").innerHTML = guessedLetters.join(' ');
+	document.getElementById("instructions").innerHTML =  "Press 2 Enter A Letter, Press 1 to pick a different word, Or Press 0 To Start Over";	
+	// 	alert("Press 2 to enter a letter");
+}
+
+//new Game
+function newGame() {
+	resetWins();
+	resetValues();
+	document.getElementById("instructions").innerHTML =  "Press The 1 Key To Generate A Random Word!";
+	document.getElementById("array of guessed letters").innerHTML = guessedLetters.join(' ');
+}
 
 			
-//the entire game
-// function HangMan_Game() {
-// 	while(game == "on"){
-// 		winCount();
-// 		guessesRemaining();
-// 		console.log(initialWord);
-// 		console.log(wordLength);
-// 		console.log(initialWordArray);
-// 		displayWords();
-// 		setGuessedArray();
-// 	}
-// }
-
-//be more specific on keys
- document.onkeyup = function(event) {
- 	var key = event.key;
- 	if (key == "2") {
-		setGuessedArray();
-		ResultLoss();
-		ResultWin();
-	}else if (key == "1") {
-		resetValues();
-		winCount();
-		pickWord(potentialWordsArray);
-		console.log(initialWord);
-		displayWords();
-		guessesRemaining();
-		alert("press 2 to enter a letter");
-	}else if (key == "0") {
-		resetWins();
-		alert("press 1 to generate random word");
+// the entire game
+function HangMan_Game() {
+		
+		document.onkeyup = function(event) {
+	 	var key = event.key;
+	 	if (key == "1") {
+	 		newWord();
+	 		console.log(guessedLetters);
+	 	}else if (key == "2") {
+			setGuessedArray();	
+			ResultLoss();
+			ResultWin();
+			//ResultTie();			
+		}else if (key == "0") {
+			newGame();
+		}
 	}
 }
+
+
+HangMan_Game();
